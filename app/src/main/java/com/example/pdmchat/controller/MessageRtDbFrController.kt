72760854b4ce3d406c9e139bff7a0abaaddc24db
SauloFernando99 <saulofernando114@gmail.com>
2024-06-12 19:16:47 -0300

    refactor: small refactor for better clarity

--- app/src/main/java/com/example/pdmchat/controller/MessageRtDbFrController.kt
+++ app/src/main/java/com/example/pdmchat/controller/MessageRtDbFrController.kt
@@ -1,36 +1,24 @@
 package com.example.pdmchat.controller
 
-import android.util.Log
 import android.os.Message
 import com.example.pdmchat.MainActivity
-import com.example.pdmchat.model.ChatDao
-import com.example.pdmchat.model.ChatDaoRtDbFrImpl
+import com.example.pdmchat.model.MessageDao
+import com.example.pdmchat.model.MessageDaoRtDbFrImpl
 
 
-class ChatRtDbFrController (private val mainActivity: MainActivity) {
-    private val chatDaoImpl: ChatDao = ChatDaoRtDbFrImpl()
+class MessageRtDbFrController(private val mainActivity: MainActivity) {
+    private val messageDaoImpl: MessageDao = MessageDaoRtDbFrImpl()
 
     fun getChats() {
-        Log.d("ChatRtDbFrController", "Iniciando obtenção de chats do Firebase")
-        val chatList = chatDaoImpl.retrieveChats()
-        Log.d("ChatRtDbFrController", "Chats obtidos do Firebase: $chatList")
+        val messageList = messageDaoImpl.listMessages()
 
-        if (chatList.isNotEmpty()) {
-            val sortedChatList = chatList.sortedByDescending { it.getDateTime() }
-            chatList.clear()
-            chatList.addAll(sortedChatList)
+        if (messageList.isNotEmpty()) {
+            val sortedMessageList = messageList.sortedByDescending { it.getDateTime() }
             mainActivity.uiUpdaterHandler.sendMessage(
                 Message.obtain().apply {
-                    data.putParcelableArrayList(
-                        "CHAT_ARRAY",
-                        ArrayList(sortedChatList)
-                    )
+                    data.putParcelableArrayList("MESSAGE_ARRAY", ArrayList(sortedMessageList))
                 }
             )
-            Log.d("ChatRtDbFrController", "Encontrados ${sortedChatList.size} chats no Firebase")
-        } else {
-            Log.d("ChatRtDbFrController", "Nenhum chat encontrado no Firebase")
         }
     }
-
 }
