3f6f17d905c8e60ef2bc51655bae3d2e201c453a
SauloFernando99 <saulofernando114@gmail.com>
2024-06-12 10:41:10 -0300

    feat: initial configuration for MessageActivity

--- app/src/main/java/com/example/pdmchat/MessageActivity.kt
+++ app/src/main/java/com/example/pdmchat/MessageActivity.kt
@@ -1,4 +1,22 @@
 package com.example.pdmchat
 
-class MessageActivity {
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pdmchat.databinding.ActivityMessageBinding
+
+class MessageActivity: AppCompatActivity() {
+
+    private val amb: ActivityMessageBinding by lazy {
+        ActivityMessageBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+        amb.toolbarIn.toolbar.apply {
+            subtitle = this@MessageActivity.javaClass.simpleName
+            setSupportActionBar(this)
+        }
+    }
 }
