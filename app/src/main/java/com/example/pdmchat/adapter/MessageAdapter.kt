892535e90dcaacd0abac405c83ac644be1c50c9f
SauloFernando99 <saulofernando114@gmail.com>
2024-06-12 13:53:29 -0300

    feat: new adapter for message

--- app/src/main/java/com/example/pdmchat/adapter/MessageAdapter.kt
+++ app/src/main/java/com/example/pdmchat/adapter/MessageAdapter.kt
@@ -1,4 +1,46 @@
 package com.example.pdmchat.adapter
 
-class MessageAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.example.pdmchat.R
+import com.example.pdmchat.model.Message
+
+class MessageAdapter(context: Context, private val messageList: MutableList<Message>):
+    ArrayAdapter<Message>(context, R.layout.tile_message, messageList){
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val message = messageList[position]
+
+        var messageTileView = convertView
+        if (messageTileView == null) {
+            messageTileView = LayoutInflater.from(context).inflate(
+                R.layout.tile_message,
+                parent,
+                false
+            ).apply{
+                tag = TileContactHolder(
+                    findViewById(R.id.senderTv),
+                    findViewById(R.id.messageTv),
+                    findViewById(R.id.datetimeTv)
+                )
+            }
+        }
+
+        (messageTileView?.tag as TileContactHolder).apply{
+            senderTv.text = message.sender
+            messageTv.text = message.text
+            messageTv.text = message.getDateTime().toString()
+        }
+
+        return messageTileView
+    }
+
+    private data class TileContactHolder(
+        val senderTv: TextView, val messageTv: TextView, val datetimeTv: TextView
+    )
+
 }
