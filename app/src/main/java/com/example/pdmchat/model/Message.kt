5292c446a1068b64fd622f38781e585bbe847ec7
SauloFernando99 <saulofernando114@gmail.com>
2024-06-12 13:22:46 -0300

    feat: implemented Message data class

---
+++ app/src/main/java/com/example/pdmchat/model/Message.kt
@@ -0,0 +1,36 @@
+package com.example.pdmchat.model
+
+import android.os.Parcelable
+import androidx.annotation.NonNull
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+@Parcelize
+@Entity
+data class Message(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = -1,
+    @NonNull
+    var sender: String = "",
+    @NonNull
+    var reciever: String = "",
+    @NonNull
+    var text: String = "",
+    @NonNull
+    var time: String = "",
+    @NonNull
+    var hour: String = "",
+): Parcelable{
+    fun getDateTime(): Date? {
+        val format = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault())
+        return try {
+            format.parse("$time $hour")
+        } catch (e: Exception) {
+            null
+        }
+    }
+}
