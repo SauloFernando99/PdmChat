ef5a0f0c907f6255fdfc41324d44f4dfada5d75a
SauloFernando99 <saulofernando114@gmail.com>
2024-06-12 19:07:32 -0300

    feat: updated the adapter and made some refactoring

--- app/src/main/java/com/example/pdmchat/adapter/MessageAdapter.kt
+++ app/src/main/java/com/example/pdmchat/adapter/MessageAdapter.kt
@@ -1,47 +1,40 @@
 package com.example.pdmchat.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
-import com.example.pdmchat.model.Chat
+import com.example.pdmchat.model.Message
 import com.example.pdmchat.R
 
-class ChatAdapter(context: Context, private val chatList: MutableList<Chat>):
-    ArrayAdapter<Chat>(context, R.layout.tile_chat, chatList) {
+class MessageAdapter(context: Context, private val messageList: MutableList<Message>) :
+    ArrayAdapter<Message>(context, R.layout.tile_message, messageList) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val chat = chatList[position]
-        val chatTileView: View
-        val holder: TileContactHolder
-
-        if (convertView == null) {
-            chatTileView = LayoutInflater.from(context).inflate(R.layout.tile_chat, parent, false)
-            holder = TileContactHolder(
-                chatTileView.findViewById(R.id.nameTv),
-                chatTileView.findViewById(R.id.chatTv),
-                chatTileView.findViewById(R.id.dateTimeTv)
-            )
-            chatTileView.tag = holder
-        } else {
-            chatTileView = convertView
-            holder = chatTileView.tag as TileContactHolder
-        }
+        val message = messageList[position]
+        val chatTileView = convertView ?: LayoutInflater.from(context).inflate(R.layout.tile_message, parent, false)
+        val holder = convertView?.tag as? TileContactHolder ?: TileContactHolder(
+            chatTileView.findViewById(R.id.nameTv),
+            chatTileView.findViewById(R.id.messageTv),
+            chatTileView.findViewById(R.id.dateTimeTv)
+        )
 
         holder.apply {
-            nameTv.text = chat.remetente
-            chatTv.text = chat.chat
-            dateTimeTv.text = "${chat.data} ${chat.horario}"
+            nameTv.text = message.sender
+            messageTv.text = message.message
+            dateTimeTv.text = "${message.date} ${message.time}"
         }
 
+        chatTileView.tag = holder
         return chatTileView
     }
 
     private data class TileContactHolder(
         val nameTv: TextView,
-        val chatTv: TextView,
+        val messageTv: TextView,
         val dateTimeTv: TextView
     )
 }
