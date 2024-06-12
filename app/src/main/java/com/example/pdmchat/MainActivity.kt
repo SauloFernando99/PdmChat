93a0c00e476bd86a0a656203ee7fcc8af3602eeb
SauloFernando99 <saulofernando114@gmail.com>
2024-06-12 10:41:48 -0300

    feat: menu configuration to navigate throught screens

--- app/src/main/java/com/example/pdmchat/MainActivity.kt
+++ app/src/main/java/com/example/pdmchat/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.pdmchat
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -26,5 +28,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.sendMessage -> {
+                val intent = Intent(this, MessageActivity::class.java)
+                //intent.putExtra("remetente", usernameInput.text.toString().trim())
+                startActivity(intent)
+                true
+            }
+            R.id.closeApp -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
